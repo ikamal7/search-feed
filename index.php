2803643af5a64e1da086e29f7a0a9a788d4a52e9
Kamal Hosen <kamalhosen8920@gmail.com>
2022-06-11 21:31:16 +0600

    Search form added

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Search</title>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
+<!--    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <div class="row height d-flex justify-content-center align-items-center">
+
+        <div class="col-md-8">
+            <form action="https://feed.lookbox.net/s1151">
+                <div class="search">
+    <!--                <i class="fa fa-search"></i>-->
+                    <input type="text" class="form-control" placeholder="Search" name="q">
+                    <button class="btn btn-primary">Search</button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+</div>
+
+<!--<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+
+</body>
+</html>
